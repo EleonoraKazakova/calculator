d20a6149eec99bee49d1013d6adea07e0d795fd1
Eleonora Kazakova <abei-2005@yandex.ru>
2022-06-10 16:48:28 +0200

    Create currentNumbers-state

--- src/InputField.tsx
+++ src/InputField.tsx
@@ -1,28 +1,35 @@
 import * as React from "react";
+import "./styles/app";
+
 interface InputFieldProps {
   value: number | string;
   inputNumber: React.Dispatch<React.SetStateAction<number | string>>;
   runOperation: React.Dispatch<React.SetStateAction<number | string>>;
+  setCurrentNumbers: React.Dispatch<React.SetStateAction<(number | string)[]>>;
+  currentNumbers: (number | string)[];
+  setCurrentSign: React.Dispatch<React.SetStateAction<number | string>>;
 }
 
 export default function InputField({
   value,
-  inputNumber,
-  runOperation,
+  setCurrentNumbers,
+  currentNumbers,
+  setCurrentSign,
 }: InputFieldProps) {
-  console.log(value);
   return (
     <input
       value={value}
       type="button"
+      className="app-button"
       onClick={() =>
         value === "+" ||
         value === "-" ||
         value === "*" ||
         value === "/" ||
-        value === "DEL"
-          ? runOperation(value)
-          : inputNumber(value)
+        value === "DEL" ||
+        value === "="
+          ? setCurrentSign(value)
+          : setCurrentNumbers([...currentNumbers, value])
       }
     />
   );
--- src/app.tsx
+++ src/app.tsx
@@ -6,54 +6,64 @@ import InputField from "./InputField";
 export default function App() {
   const [result, setResult] = React.useState<(string | number)[]>([]);
   const [input, setInput] = React.useState<(string | number)[]>([0]);
+  const [currentNumbers, setCurrentNumbers] = React.useState<
+    (number | string)[]
+  >([]);
   const [currentSign, setCurrentSign] = React.useState<string>("");
+  const [showResult, setShowResult] = React.useState<boolean>(false);
 
   function inputNumber(num: number | string) {
     input[0] === 0 ? input.shift() : null;
     setInput([...input, num]);
   }
 
-  function runOperation(sign: string) {
+  function runOperation(sign: string): void {
     setCurrentSign(sign);
+
     setInput([]);
     if (result.length > 0 && input.length > 0) {
-      setResult([calculate(currentSign)]);
+      calculate(currentSign);
     } else {
       setResult(input);
     }
     console.log("calculate(sign):", calculate(sign));
   }
 
-  const calculate = (sign: string): number => {
+  function calculate(sign: string): void {
+    console.log("result1:", result);
     if (sign === "/") {
-      return Number(result.join("")) / Number(input.join(""));
+      setResult([
+        (Number(result.join("")) / Number(input.join(""))).toString(),
+      ]);
     } else if (sign === "x") {
-      return Number(result.join("")) * Number(input.join(""));
+      setResult([
+        (Number(result.join("")) * Number(input.join(""))).toString(),
+      ]);
     } else if (sign === "+") {
-      return Number(result.join("")) + Number(input.join(""));
+      setResult([
+        (Number(result.join("")) + Number(input.join(""))).toString(),
+      ]);
     } else if (sign === "-") {
-      return Number(result.join("")) - Number(input.join(""));
+      setResult([
+        (Number(result.join("")) - Number(input.join(""))).toString(),
+      ]);
+      setResult([input.join("")]);
     } else if (sign === "=") {
-      return Number(result);
+      setResult([
+        (Number(result.join("")) + Number(input.join(""))).toString(),
+      ]);
     }
-  };
+  }
 
   const blockOne = numbersData.allNumbers.map((value: number | string) => (
     <InputField
       value={value}
       runOperation={runOperation}
       inputNumber={inputNumber}
+      setCurrentNumbers={setCurrentNumbers}
+      currentNumbers={currentNumbers}
+      setCurrentSign={setCurrentSign}
     />
-    /*<input
-      type="button"
-      value={el}
-      className="app-button"
-      onClick={(event) =>
-        el === "+"
-          ? runOperation(event.currentTarget.value)
-          : inputNumber(event.currentTarget.value)
-      }
-    />*/
   ));
 
   const reset = () => {
@@ -62,9 +72,10 @@ export default function App() {
     setCurrentSign("");
   };
 
-  console.log("1: ", result);
-  console.log("2: ", input);
-  console.log("sign: ", currentSign);
+  console.log("currentNumbers: ", currentNumbers);
+
+  console.log("result: ", result);
+  console.log("currentSign: ", currentSign);
 
   return (
     <div className="app-grid">
@@ -72,7 +83,7 @@ export default function App() {
       <div className="app-content">
         <div className="app-display">
           <p>
-            {result.join("")}
+            {result}
             {currentSign}
             {input.join("")}
           </p>
@@ -80,53 +91,6 @@ export default function App() {
 
         <div className="app-block-buttons">
           <div className="app-line-buttons">{blockOne}</div>
-
-          <div className="app-line-buttons">
-            <div className="app-button" onClick={() => inputNumber(4)}>
-              4
-            </div>
-            <div className="app-button" onClick={() => inputNumber(5)}>
-              5
-            </div>
-            <div className="app-button" onClick={() => inputNumber(6)}>
-              6
-            </div>
-            <div className="app-button" onClick={() => runOperation("+")}>
-              +
-            </div>
-          </div>
-
-          <div className="app-line-buttons">
-            <input
-              type="button"
-              value={1}
-              className="app-button"
-              onClick={(event) => inputNumber(event.currentTarget.value)}
-            />
-
-            <div className="app-button" onClick={() => inputNumber(2)}>
-              2
-            </div>
-            <div className="app-button" onClick={() => inputNumber(3)}>
-              3
-            </div>
-            <div className="app-button" onClick={() => runOperation("-")}>
-              -
-            </div>
-          </div>
-
-          <div className="app-line-buttons">
-            <div className="app-button" onClick={() => inputNumber(".")}>
-              .
-            </div>
-            <div className="app-button" onClick={() => inputNumber(0)}>
-              0
-            </div>
-            <div className="app-button">DEL</div>
-            <div className="app-button" onClick={() => runOperation("x")}>
-              x
-            </div>
-          </div>
 
           <div className="app-line-buttons-long">
             <div className="app-button-long-blue" onClick={reset}>
